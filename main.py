bee5e66e120e5880d0b3ecba6a868275bc56a18d
lmlcvc <lana.milicevic10@gmail.com>
2023-06-05 17:45:36 +0200

    WIP: GCN classification model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import networkx as nx
 from stellargraph.mapper import PaddedGraphGenerator
 from tensorboard.notebook import display
+import tensorflow as tf
 
 from util import file_utils as fu, graph_utils as gu
 from model import model as md
@@ -8,20 +9,26 @@ from model import model as md
 import os
 import configparser
 import pandas as pd
-
-from stellargraph import datasets
-import tensorflow as tf
+import logging
+import warnings
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 config = config['default']
 
 check_setup = config['check_setup']
+demo_run = config['demo_run']
 
 targets_dir = config['targets_dir']
 pdb_catalytic_dir = config['pdb_catalytic_dir']
 pdb_non_catalytic_dir = config['pdb_non_catalytic_dir']
+pdb_demo_dir = config['pdb_demo_dir']
 graph_dir = config['graph_dir']
+graph_demo_dir = config['graph_demo_dir']
+
+# suppress "FutureWarning: The default value of regex will change from True to False in a future version." for graph
+# generation
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 if __name__ == "__main__":
     # run setup check
@@ -30,6 +37,7 @@ if __name__ == "__main__":
     # check if files have been transformed
     if not os.path.isdir(targets_dir) or not os.listdir(targets_dir):
         fu.generate_targets()
+        logging.info("Finished target generation")
 
     # # graph generation
     # if not os.path.isdir(graph_dir):
@@ -45,25 +53,34 @@ if __name__ == "__main__":
     # load graphs and labels to pass to the model
     # graphs = [gu.load_graph(graph_dir, graph) for graph in os.listdir(graph_dir)]
 
-    graph = gu.generate_graph_direct(pdb_catalytic_dir, os.listdir(pdb_catalytic_dir)[0].replace(".pdb", ""))
-    print(graph.info())
-    print(graph.nodes())
+    if demo_run == "Y" or demo_run == "y":
+        graphs = [gu.generate_graph_direct(pdb_demo_dir, entry.replace(".pdb", "")) for entry in
+                  os.listdir(pdb_demo_dir)]
+        logging.info("Generated demo graphs")
+    else:
+        graphs_catalytic = [gu.generate_graph_direct(pdb_catalytic_dir, entry.replace(".pdb", "")) for entry in
+                            os.listdir(pdb_catalytic_dir)]
+        logging.info("Generated catalytic graphs")
 
-    # graphs_catalytic = [gu.generate_graph_direct(pdb_catalytic_dir, entry.replace(".pdb", "")) for entry in
-    #                     os.listdir(pdb_catalytic_dir)]
-    #
-    # graphs_non_catalytic = [gu.generate_graph_direct(pdb_non_catalytic_dir, entry.replace(".pdb", "")) for entry in
-    #                         os.listdir(pdb_non_catalytic_dir)]
-    #
-    # graphs = graphs_catalytic + graphs_non_catalytic
+        graphs_non_catalytic = [gu.generate_graph_direct(pdb_non_catalytic_dir, entry.replace(".pdb", "")) for entry in
+                                os.listdir(pdb_non_catalytic_dir)]
+        logging.info("Generated non-catalytic graphs")
 
-    # graph_labels = gu.load_graph_labels()
-    # gu.graphs_summary(graphs, graph_labels)
-    # graph_labels.value_counts().to_frame()
-    # graph_labels = pd.get_dummies(graph_labels, drop_first=True)
-    # # Adapt graphs to Keras model
-    # graph_generator = PaddedGraphGenerator(graphs=graphs)
+        graphs = graphs_catalytic + graphs_non_catalytic
 
-    # Create classification model
-    # model = md.create_graph_classification_model_gcn(graph_generator)
-    # model = md.create_graph_classification_model_dcgnn(graph_generator)
+    graph_labels = gu.load_graph_labels()
+    gu.graphs_summary(graphs, graph_labels)
+    graph_labels.value_counts().to_frame()
+    graph_labels = pd.get_dummies(graph_labels, drop_first=True)
+
+    # Adapt graphs to Keras model
+    graph_generator = PaddedGraphGenerator(graphs=graphs)
+    logging.info("Graphs adapted for model")
+
+    # Create classification models
+    model_gcn = md.create_graph_classification_model_gcn(graph_generator)
+    logging.info("Created GCN model")
+    print(model_gcn.summary())
+
+    # model_dcgnn = md.create_graph_classification_model_dcgnn(graph_generator)
+    # logging.info("Created DCGNN model")
--- model/model.py
+++ model/model.py
@@ -36,13 +36,20 @@ def create_graph_classification_model_gcn(generator):
         dropout=0.5,
     )
     x_inp, x_out = gc_model.in_out_tensors()
-    predictions = Dense(units=32, activation="relu")(x_out)
-    predictions = Dense(units=16, activation="relu")(predictions)
-    predictions = Dense(units=1, activation="sigmoid")(predictions)
+    # Only dicts, lists, and tuples of input tensors are supported. Nested inputs are not supported (e.g. lists of list or dicts of dict)
+    print(type(x_inp), type(x_out))
+    print(x_inp)
+
+    # for tensor in x_inp:
+    #     tf.print(tensor)
+
+    predictions = tf.keras.layers.Dense(units=32, activation="relu")(x_out)
+    predictions = tf.keras.layers.Dense(units=16, activation="relu")(predictions)
+    predictions = tf.keras.layers.Dense(units=1, activation="sigmoid")(predictions)
 
     # Create the Keras model and prepare it for training
-    model = Model(inputs=x_inp, outputs=predictions)
-    model.compile(optimizer=Adam(0.005), loss=binary_crossentropy, metrics=["acc"])
+    model = tf.keras.Model(inputs=x_inp, outputs=predictions)
+    model.compile(optimizer=tf.keras.optimizers.Adam(0.005), loss=binary_crossentropy, metrics=["acc"])
 
     return model
 
